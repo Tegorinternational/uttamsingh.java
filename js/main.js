c59acfbce645b8e56153080a25ddff689fcdf9b0
Tegor International <104412526+Tegorinternational@users.noreply.github.com>
2023-06-10 13:42:59 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
- fetch("projects.json")
+ fetch("https://cdn.jsdelivr.net/gh/Tegorinternational/uttamsingh.java/projects.json")
   .then((response) => response.json())
   .then((data) => {
     // Get the div element to append the cards to
